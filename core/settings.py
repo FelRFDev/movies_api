41593cad7f528587153d874cc0ae0c652c18452b
felrfdev <comunidadehawks@gmail.com>
2025-09-10 14:45:17 -0300

    feat: add permission classes and global default permission to views.

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    'rest_framewwork_simplejwt',''
+    'rest_framework_simplejwt',''
 
     'authentication',
     'genres',
